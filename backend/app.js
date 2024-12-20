12dbc02447003601f13d37f27f985d98afd9e8a5
SimplyKanika <kanikabhuvad9@gmail.com>
2024-12-20 20:40:51 +0530

    integration tailwindcss

---
+++ backend/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corsOption = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOption));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/",(req,res) => {
+    res.json({ message: "Welcome to the Website "})
+})
+
+//initializing the db in the main server
+const db = require("./src/models");
+db.sequelize.sync()
+   .then(() => {
+    console.log("synced db");
+   })
+   .catch((err) => {
+    console.log("Failed to sync db"+err.message);
+   });
+
+   //With all the commentend part I was kinding of facing some issue so deleted that part
+// db.sequelize.sync({ force: true }).then(() =>{
+//     console.log("Drop and re-sync db.");
+// });
+
+// sequelize.sync({ force: true }) // Force sync to recreate tables
+//     .then(() => {
+//         console.log("Database & tables created!");
+//     })
+
+
+//IMPORTING AND USING ROUTES
+const signupRoutes = require('./src/routes/signup.route');
+app.use('/api/signup', signupRoutes);
+
+const loginRoutes = require('./src/routes/login.route');
+app.use('/api/login', loginRoutes);
+
+//Importing Dashboard routes
+// const dashboardRoutes = require('./src/routes/dashboard.route');
+// app.use('/api/dashboard', dashboardRoutes);
+ const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, () => {
+    console.log(` server running at port ${PORT}  `);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppRoutes from './Routes/Routes.js';
 
+
 const App = () => {
   return <AppRoutes />;
 };
