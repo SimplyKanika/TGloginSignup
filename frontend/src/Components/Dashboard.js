b6eba21e1abc0d00a58826d3132c214a216fd127
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-01-01 19:47:35 +0530

    Added the whole frontend of the Dashboard also did integration of the frontend code with the backend code

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -46,6 +46,49 @@ const Dashboard = () => {
     }
   };
 
+  return(
+    <div>
+      <h1>Dashboard</h1>
+      <form onSubmit={handleUpload}>
+        <div>
+          <label>Student Foregin ID:</label>
+          <input
+          type='text'
+          value={studentForeginId}
+          onChange={(e) => setStudentForeginId(e.target.value)}
+          required 
+          />
+        </div>
+        <div>
+          <label>Document Type:</label>
+          <select value={documentType} onChange={(e) => setDocumentType(e.target.value)} required>
+            <option value='result_12th'>12th Result</option>
+            <option value='aadharCard'>Aadhar Card</option>
+            <option value='panCard'>PAN Card</option>
+            <option value='mhtcetResult'>MHT-CET Result</option>
+            <option value='admissionCard'>Admission Card</option>
+            <option value='capCard'>CAP Card</option>
+            <option value='domicile'>Domicile</option>
+            <option value='birthCertificate'>Birth Certificate</option>
+            <option value='leavingCertificate'>Leaving Certificate</option>
+          </select>
+        </div>
+        <div>
+          <label>File: </label>
+          <input type='file' onChange={handleFileChange} required />
+        </div>
+        <button type='submit'>Upload</button>
+      </form>
+      <h2>Uploaded Documents</h2>
+      <ul>
+        {documents.map((doc) => (
+          <li key={doc.id}>
+            {doc.documentType}: <a href={`http://localhost:8080/${doc.filePath}`} target='_blank' rel='noopener noreferrer'> View </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Dashboard
